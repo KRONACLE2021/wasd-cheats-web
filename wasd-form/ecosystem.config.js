c195de73c116c91e1554518b96d8700c7b55b015
astrid <killmeuwu@protonmail.com>
2021-10-17 00:33:25 +0000

    updated for prod

---
+++ wasd-form/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'wasd-forum',
+      cwd: './',
+      script: 'npm',
+      args: 'start',
+      env: {
+      },
+    },
+    // optionally a second project
+],};
