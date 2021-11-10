f35c7fcc5eeff5290f88396630ac5ecdaadae98a
Windows <killmeuwu@protonmail.com>
2021-11-10 15:59:35 -0800

    Update config.ts

--- wasd-back/config.ts
+++ wasd-back/config.ts
@@ -1,1 +1,5 @@
-export const HCAPTHCA_SECRET = "0x0000000000000000000000000000000000000000";
+//@ts-ignore
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const HCAPTHCA_SECRET = process.env.HCAPTCHA_SECRET ? process.env.HCAPTCHA_SECRET : "0x0000000000000000000000000000000000000000";
